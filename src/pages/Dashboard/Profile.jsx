316b1c0ef4cda595a417fc55e5e917b62df19fcd
Benjamin Faruna <benjaminfaruna@gmail.com>
2024-06-01 02:36:03 +0100

    Profile page integration

--- src/pages/Dashboard/Profile.jsx
+++ src/pages/Dashboard/Profile.jsx
@@ -5,38 +5,55 @@ import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
 import React, { useState } from 'react';
 
+import { useWeb3ModalAccount } from "@web3modal/ethers/react";
+
+import { formatUnits } from "ethers";
+
+import UseFetchRequests from "../../Hooks/UseFetchRequests";
+import useLocalStorage from "../../Hooks/useLocalStorage";
+import useFetchUserCollateral from "../../Hooks/useFetchUserCollateral";
+
+import TokenList from "../../constants/tokenList.json"
+
 const Profile = () => {
+  const { address } = useWeb3ModalAccount();
   const [value, setValue] = useState("My Requests");
+
+  const collateral = useFetchUserCollateral();
+  const requests = UseFetchRequests();
+
+  const [storedValue,] = useLocalStorage("passportScore");
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+
   return (
     <main>
-        <h2 className="lg:text-[24px] md:text-[24px] text-[20px] mb-6 flex justify-between">User Data <span className="text-[16px]">ebukiz@gmail.com</span></h2>
-        <section className="flex lg:flex-row md:flex-row flex-col justify-between">
-            <div className="bg-bg-gray border border-bg-ash/50 p-6 rounded-lg w-[100%] lg:w-[23%] md:w-[23%]">
-                <h3>Gitcoin Score</h3>
-                <p>5</p>
-            </div>
-            <div className="bg-bg-gray border border-bg-ash/50 p-6 rounded-lg w-[100%] lg:w-[23%] md:w-[23%]">
-                <h3>Total Collateral</h3>
-                <p>5</p>
-            </div>
-            <div className="bg-bg-gray border border-bg-ash/50 p-6 rounded-lg w-[100%] lg:w-[23%] md:w-[23%]">
-                <h3>Total Requests</h3>
-                <p>5</p>
-            </div>
-            <div className="bg-bg-gray border border-bg-ash/50 p-6 rounded-lg w-[100%] lg:w-[23%] md:w-[23%]">
-                <h3>Total Offer</h3>
-                <p>5</p>
-            </div>
-        </section>
-        <section>
-            <h2 className="lg:text-[24px] md:text-[24px] text-[20px] mt-8">Transaction History</h2>
+      <h2 className="lg:text-[24px] md:text-[24px] text-[20px] mb-6 flex justify-between">User Data <span className="text-[16px]">ebukiz@gmail.com</span></h2>
+      <section className="flex lg:flex-row md:flex-row flex-col justify-between">
+        <div className="bg-bg-gray border border-bg-ash/50 p-6 rounded-lg w-[100%] lg:w-[23%] md:w-[23%]">
+          <h3>Gitcoin Score</h3>
+          <p>{storedValue}</p>
+        </div>
+        <div className="bg-bg-gray border border-bg-ash/50 p-6 rounded-lg w-[100%] lg:w-[23%] md:w-[23%]">
+          <h3>Total Collateral</h3>
+          <p><span>&#36;</span>{parseFloat(formatUnits(collateral, 18)).toFixed(2)}</p>
+        </div>
+        <div className="bg-bg-gray border border-bg-ash/50 p-6 rounded-lg w-[100%] lg:w-[23%] md:w-[23%]">
+          <h3>Total Requests</h3>
+          <p>5</p>
+        </div>
+        <div className="bg-bg-gray border border-bg-ash/50 p-6 rounded-lg w-[100%] lg:w-[23%] md:w-[23%]">
+          <h3>Total Offer</h3>
+          <p>5</p>
+        </div>
+      </section>
+      <section>
+        <h2 className="lg:text-[24px] md:text-[24px] text-[20px] mt-8">Transaction History</h2>
 
-            <Box sx={{ width: "100%", typography: "body1" }}>
+        <Box sx={{ width: "100%", typography: "body1" }}>
           <TabContext value={value}>
             <Box
               sx={{
@@ -61,29 +78,37 @@ const Profile = () => {
               </TabList>
             </Box>
             <TabPanel value="My Requests">
-            <div className="w-[100%] lg:w-[31%] md:w-[31%] rounded-lg border border-bg-ash/35 bg-bg-gray p-4 mt-6">
-                <img src="https://img.freepik.com/free-vector/3d-cashback-with-flying-gold-coins-dollar-bill_88138-974.jpg?w=1800&t=st=1717184738~exp=1717185338~hmac=40c12b1e890a608add26b80343d64ed3f04dcf8ce411fada1dcb42a9c0c7b172" alt="" className="w-[100%] rounded-lg h-[200px] object-cover object-center mb-4"/>
-                <p>Amount: </p>
-                <p>Rate</p>
-                <p>Repayment</p>
-                <p>Return date: <span>3 Days: 12Hrs : 02Secs</span></p>
-            </div>
+              {
+                requests.map((data, index) => {
+                  if (data?.address == address) {
+                    return (
+                      <div key={index} className="w-[100%] lg:w-[31%] md:w-[31%] rounded-lg border border-bg-ash/35 bg-bg-gray p-4 mt-6">
+                        <img src="https://img.freepik.com/free-vector/3d-cashback-with-flying-gold-coins-dollar-bill_88138-974.jpg?w=1800&t=st=1717184738~exp=1717185338~hmac=40c12b1e890a608add26b80343d64ed3f04dcf8ce411fada1dcb42a9c0c7b172" alt="" className="w-[100%] rounded-lg h-[200px] object-cover object-center mb-4" />
+                        <p>Amount: {formatUnits(data?.amount, TokenList[data?.loanReq]?.decimals)}</p>
+                        <p>Rate: {data?.interest.toString()}<span>&#37;</span></p>
+                        <p>Repayment: {formatUnits(data?.repayment, TokenList[data?.loanReq]?.decimals + 1)}</p>
+                        <p>Return date: <span>{(new Date(Number(data?.rDate) * 1000)).toLocaleString()}</span></p>
+                      </div>
+                    )
+                  }
+                })
+              }
             </TabPanel>
             <TabPanel value="My Offers">
-            <div className="flex justify-between">
-            <div className="w-[100%] lg:w-[31%] md:w-[31%] rounded-lg border border-bg-ash/35 bg-bg-gray p-4 mt-6">
-                <img src="https://img.freepik.com/free-photo/3d-render-businessman-hand-holding-money-banknotes_107791-17027.jpg?size=626&ext=jpg" alt="" className="w-[100%] rounded-lg h-[200px] object-cover object-center mb-4"/>
-                <p className="truncate">Lender: </p>
-                <p>Amount: </p>
-                <p>Rate: </p>
-                <p>Repayment: </p>
-                <p>Return date: <span>3 Days: 12Hrs : 02Secs</span></p>
-            </div>    
-            </div>
+              <div className="flex justify-between">
+                <div className="w-[100%] lg:w-[31%] md:w-[31%] rounded-lg border border-bg-ash/35 bg-bg-gray p-4 mt-6">
+                  <img src="https://img.freepik.com/free-photo/3d-render-businessman-hand-holding-money-banknotes_107791-17027.jpg?size=626&ext=jpg" alt="" className="w-[100%] rounded-lg h-[200px] object-cover object-center mb-4" />
+                  <p className="truncate">Lender: </p>
+                  <p>Amount: </p>
+                  <p>Rate: </p>
+                  <p>Repayment: </p>
+                  <p>Return date: <span>3 Days: 12Hrs : 02Secs</span></p>
+                </div>
+              </div>
             </TabPanel>
           </TabContext>
-        </Box>       
-        </section>
+        </Box>
+      </section>
     </main>
   )
 }
